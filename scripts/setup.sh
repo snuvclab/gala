3cfdd884b54f673fa5177f4935343b285dc1c2bf
Taeksoo Kim <taeksu98@snu.ac.kr>
2024-01-24 16:53:01 +0900

    smplx setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -30,7 +30,7 @@ rm -rf Grounded-Segment-Anything
 
 # SMPL-X
 # Script from https://github.com/yfeng95/SCARF/blob/main/fetch_data.sh
-mkdir -p ./deformer/dataset
+mkdir -p ./deformer/data
 # SMPL-X 2020 (neutral SMPL-X model with the FLAME 2020 expression blendshapes)
 urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 echo -e "\nYou need to register at https://smpl-x.is.tue.mpg.de"
@@ -38,13 +38,13 @@ read -p "Username (SMPL-X):" username
 read -p "Password (SMPL-X):" password
 username=$(urle $username)
 password=$(urle $password)
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O './deformer/dataset/SMPLX_NEUTRAL_2020.npz' --no-check-certificate --continue
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O './deformer/data/SMPLX_NEUTRAL_2020.npz' --no-check-certificate --continue
 
 # scarf utilities
 echo -e "\nDownloading data..."
-wget https://owncloud.tuebingen.mpg.de/index.php/s/n58Fzbzz7Ei9x2W/download -O ./deformer/dataset/scarf_utilities.zip
-unzip ./deformer/dataset/scarf_utilities.zip -d ./deformer/dataset
-rm ./deformer/dataset/scarf_utilities.zip
+wget https://owncloud.tuebingen.mpg.de/index.php/s/n58Fzbzz7Ei9x2W/download -O ./deformer/data/scarf_utilities.zip
+unzip ./deformer/data/scarf_utilities.zip -d ./deformer/data
+rm ./deformer/data/scarf_utilities.zip
 
 
 # Generate tetrahedral grid
